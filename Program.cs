b0ba9388e3f74a87ebf85ce5c5e79a85231ce87e
Pritesh Zaver <priteshzaver@gmail.com>
2023-02-26 17:28:10 -0600

    phase1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿using System;
 
+Console.WriteLine("Hello!");
+Console.Write("Please guess a number: ");
+string userGuess = Console.ReadLine();
+double userNumber = Double.Parse(userGuess);
+Console.WriteLine($"You entered {userNumber}");
