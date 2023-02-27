564a02cf33ea7fd1429a23c8f9f9e08c4c598a43
Pritesh Zaver <priteshzaver@gmail.com>
2023-02-26 21:32:21 -0600

    phase7

--- Program.cs
+++ Program.cs
@@ -1,34 +1,64 @@
 ﻿using System;
 
+Main();
 
-Console.WriteLine("Hello!");
-Console.Write($"Please guess a number: ");
-
-string userGuess = Console.ReadLine();
-double userNumber = Double.Parse(userGuess);
-
-int tryNumber = 4;
-Random r = new Random();
-int secretNumber = r.Next(1, 100);
-
-
-while (userNumber != secretNumber && tryNumber > 1)
+void Main()
 {
-    tryNumber--;
-    if (userNumber > secretNumber)
+    Console.WriteLine("Hello! To play the Guessing Game, please select a difficulty.");
+    Console.WriteLine("Easy - this gives the user eight guesses.");
+    Console.WriteLine("Medium - this gives the user six guesses.");
+    Console.WriteLine("Hard - this gives the user four guesses.");
+    string difficulty = Console.ReadLine().ToLower();
+    int tryNumber = 0;
+
+    switch (difficulty)
     {
-        Console.WriteLine("Your guess is too high!");
+        case "easy":
+            tryNumber = 8;
+            break;
+        case "medium":
+            tryNumber = 6;
+            break;
+        case "hard":
+            tryNumber = 4;
+            break;
+    }
+
+    Console.WriteLine("Please enter your guess:");
+    string userGuess = Console.ReadLine();
+    double userNumber = Double.Parse(userGuess);
+    Random r = new Random();
+    int secretNumber = r.Next(1, 100);
+
+    while (userNumber != secretNumber && tryNumber > 1)
+    {
+        tryNumber--;
+        if (userNumber > secretNumber)
+        {
+            Console.WriteLine("Your guess is too high!");
+        }
+        else
+        {
+            Console.WriteLine("Your guess is too low!");
+        }
+        Console.Write($"Try again ({tryNumber} trie(s) left): ");
+        userGuess = Console.ReadLine();
+        userNumber = Double.Parse(userGuess);
+
+    }
+    if (userNumber == secretNumber)
+    {
+        Console.WriteLine("Your guess is correct!");
     }
     else
     {
-        Console.WriteLine("Your guess is too low!");
+        Console.WriteLine("You lose!");
     }
-    Console.Write($"Try again ({tryNumber} trie(s) left): ");
-    userGuess = Console.ReadLine();
-    userNumber = Double.Parse(userGuess);
 
 }
-if (userNumber == secretNumber)
-{
-    Console.WriteLine("Your guess is correct!");
-}
+
+
+
+
+
+
