1a25cd3dbf129156a6cd669c388d6e4a49e21b70
Pritesh Zaver <priteshzaver@gmail.com>
2023-02-26 18:45:16 -0600

    phase4

--- Program.cs
+++ Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 
 
-for (int i = 1; i <= 4; i++)
+Console.WriteLine("Hello!");
+Console.Write($"Please guess a number: ");
+
+string userGuess = Console.ReadLine();
+double userNumber = Double.Parse(userGuess);
+int secretNumber = 42;
+int tryNumber = 1;
+
+while (userNumber != secretNumber && tryNumber < 4)
 {
-    Console.WriteLine("Hello!");
-    Console.Write("Please guess a number: ");
-
-    string userGuess = Console.ReadLine();
-    double userNumber = Double.Parse(userGuess);
-    int secretNumber = 42;
-
-    if (userNumber == secretNumber)
-    {
-        Console.WriteLine("Your guess is correct!");
-    }
-    else
-    {
-        Console.WriteLine("Your guess is wrong!");
-    }
+    tryNumber++;
+    Console.WriteLine("Your guess is wrong!");
+    Console.Write($"Try again (TRY #{tryNumber}): ");
+    userGuess = Console.ReadLine();
+}
+if (userNumber == secretNumber)
+{
+    Console.WriteLine("Your guess is correct!");
 }
